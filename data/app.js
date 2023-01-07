d328ece4050a420c6be8513c68dd9fd5d6546108
Valery Ramirez <valeryramirez@hotmail.com>
2023-01-07 00:32:55 -0400

    Se ajustaron los estilos para que se despliegue el contenido de la aplicación de forma correcta en varios dispositivos.
    Se tiene una falla la cual imagino es de platformio donde por alguna
    razón no me deja usar funciones javascript si no defino el archivo antes
    de usarla en el html y por otro lado no me deja hacer querys en el
    archivo .js si son llamadas desde un onclick dentro del html. Buscar por
    qué sucede.
    
    Se debe comenzar a enlazar lo hecho en la página web con la lógica del
    micro

--- data/app.js
+++ data/app.js
@@ -1,9 +1,10 @@
-//const botonBloquear = document.querySelector("#boton-salir");
-//botonBloquear.addEventListener("click", botonSalir);
-
 function botonSalir() {
     var sHTTP = new XMLHttpRequest();
     sHTTP.open("GET", "no-permitido", true);
     sHTTP.send();
-    setTimeout(function(){window.open("/autenticar", "_self");}, 1000);
+    setTimeout(function(){window.open("autenticar", "_self");}, 1000)
+}
+
+function resPantalla() {
+    return window.innerWidth;
 }
