a93dab3eb964210e5d6a2b638816d768ab75eeaf
Valery Ramirez <valeryramirez@hotmail.com>
2023-01-10 15:31:11 -0400

    Agregada la parte del tema que lo hace cambiar con transición, en este caso de 0,5 segundos. Falta todo lo demás

--- data/app.js
+++ data/app.js
@@ -1,34 +1,27 @@
+"use strict";
+
+function Cambia_Tema() {
+    const cambiador = document.querySelector('.tema-visual');
+    document.body.classList.toggle('tema-claro');
+    document.body.classList.toggle('tema-oscuro');
+    document.body.style.transition = "1s ease"; //indicado por openAi
+    const className = document.body.className;
+    if(className == 'tema-claro') {
+        document.getElementById("temVis").innerHTML = 'Oscuro';
+    }
+    else if(className == 'tema-oscuro') {
+        document.getElementById("temVis").innerHTML = 'Claro';
+    }
+}
+
 function botonSalir() {
     var sHTTP = new XMLHttpRequest();
     sHTTP.open("GET", "no-permitido", true);
     sHTTP.send();
-    setTimeout(function(){window.open("autenticar", "_self");}, 1000);
+    setTimeout(function(){window.open("autenticar.html", "_self");}, 1000)
 }
 
 function resPantalla() {
     return window.innerWidth;
-}"use strict";
-
-const cambiador = document.querySelector('.tema-visual');
-cambiador.addEventListener('click',function() {
-    document.body.classList.toggle('tema-claro');
-    document.body.classList.toggle('tema-oscuro');
-
-    const className = document.body.className;
-    console.log("Las clases son: " + className);
-    if(className == 'tema-claro') {
-        console.log('Contexto actual: ' + this);
-        console.log('HTML interno actual del botón: ' + document.getElementById("temVis"));
-        document.getElementById("temVis").innerHTML = 'Oscuro';
-    }
-    else if(className == 'tema-oscuro') {
-        console.log('Contexto de texto actual: ' + this.textContext);
-        document.getElementById("temVis").innerHTML = 'Claro';
-    }
-    else {
-
-    }
-    console.log('Clase actual: ' + className);
-    console.log('Contexto de texto: ' + this.textContext);
-});
+}
 
