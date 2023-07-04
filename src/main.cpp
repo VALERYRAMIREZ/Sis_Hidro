ff11071538e55ba0ed3bbb23770ea1d1d5eafcd3
Valery Ramirez <valeryramirez@hotmail.com>
2023-07-04 11:46:42 -0400

    Comentadas las impresiones de estado de horario de encendido del sistema con excepción de la impresión de la fecha actual y el resultado de la comparación de horarios.

--- src/main.cpp
+++ src/main.cpp
@@ -243,35 +243,28 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
   uint8_t manHoraIniProg = 0, manMinIniProg = 0, manHoraFinProg = 0, manMinFinProg = 0, tarHoraIniProg = 0, tarMinIniProg = 0, tarHoraFinProg = 0, tarMinFinProg = 0;
   Serial.print("Fecha completa: ");
   Serial.println(data);
-  if(sscanf(data, "%2d:%2d:%2d %9[^,], %s %2d %4d", &tiempoLeido.hora, &tiempoLeido.minuto, &tiempoLeido.segundo, tiempoLeido.diaSemana, tiempoLeido.mesAnio, &tiempoLeido.dia, &tiempoLeido.anio) == 7);
+  if(sscanf(data, "%2d:%2d:%2d %9[^,], %s %2d %4d", &tiempoLeido.hora, &tiempoLeido.minuto, &tiempoLeido.segundo, tiempoLeido.diaSemana, tiempoLeido.mesAnio, &tiempoLeido.dia, &tiempoLeido.anio) != 7)
   {
-    Serial.print("Año leído: ");
-    Serial.println(tiempoLeido.anio);
-    Serial.print("Mes leído: ");
-    Serial.println(tiempoLeido.mesAnio);
-    Serial.print("Día leído: ");
-    Serial.println(tiempoLeido.diaSemana);
-    Serial.print("Hora Leída: ");
-    Serial.println(tiempoLeido.hora);
-    Serial.print("Minuto leído: ");
-    Serial.println(tiempoLeido.minuto);
-
+    Serial.println("La trama de reloj recibida es incorrecta, no se puede operar en automático.");
+    comparado = false;
+    return comparado;
+  }
+  else
+  {
     if((string) tiempoLeido.diaSemana == "Monday")
     {
-      Serial.println("Leyendo programación de lunes almacenada.");
       sscanf(semana.lunes.inicioManana, "%2d:%2d", &manHoraIniProg, &manMinIniProg);
       sscanf(semana.lunes.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.lunes.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.lunes.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
@@ -280,20 +273,18 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
     }
     else if((string) tiempoLeido.diaSemana == "Tuesday")
     {
-      Serial.println("Leyendo programación de martes almacenada.");
       sscanf(semana.martes.inicioManana, "%2d:%2d", &manHoraIniProg, &manMinIniProg);
       sscanf(semana.martes.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.martes.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.martes.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
@@ -302,20 +293,18 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
     }
     else if((string) tiempoLeido.diaSemana == "Wednesday")
     {
-      Serial.println("Leyendo programación de miércoles almacenada.");
       sscanf(semana.miercoles.inicioManana, "%2d:%2d", &manHoraIniProg, &manMinIniProg);
       sscanf(semana.miercoles.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.miercoles.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.miercoles.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println("Leída programación de miércoles almacenada");
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
+      Serial.println(tarMinFinProg);*/
       
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
@@ -325,20 +314,18 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
     }
     else if((string) tiempoLeido.diaSemana == "Thursday")
     {
-      Serial.println("Leyendo programación de jueves almacenada.");
       sscanf(semana.jueves.inicioManana, "%2d:%2d", &manHoraIniProg, &manMinIniProg);
       sscanf(semana.jueves.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.jueves.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.lunes.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
@@ -351,16 +338,14 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
       sscanf(semana.viernes.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.viernes.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.lunes.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println("Leída programación de viernes almacenada.");
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
@@ -373,15 +358,14 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
       sscanf(semana.sabado.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.sabado.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.sabado.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println(manHoraIniProg);
+      /*Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
       Serial.println(manMinFinProg);
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
@@ -390,13 +374,11 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
     }
     else if((string) tiempoLeido.diaSemana == "Sunday")
     {
-      Serial.println("Leyendo programación de domingo almacenada.");
       sscanf(semana.domingo.inicioManana, "%2d:%2d", &manHoraIniProg, &manMinIniProg);
       sscanf(semana.domingo.finManana, "%2d:%2d", &manHoraFinProg, &manMinFinProg);
       sscanf(semana.domingo.inicioTarde, "%2d:%2d", &tarHoraIniProg, &tarMinIniProg);
       sscanf(semana.domingo.finTarde, "%2d:%2d", &tarHoraFinProg, &tarMinFinProg);
-      Serial.println(semana.domingo.finTarde);
-      Serial.println("Leída programación de domingo almacenada.");
+      /*Serial.println(semana.domingo.finTarde);
       Serial.println(manHoraIniProg);
       Serial.println(manMinIniProg);
       Serial.println(manHoraFinProg);
@@ -404,8 +386,7 @@ bool Compara_RTC(const char * data, Semana tPrograma) {
       Serial.println(tarHoraIniProg);
       Serial.println(tarMinIniProg);
       Serial.println(tarHoraFinProg);
-      Serial.println(tarMinFinProg);
-      
+      Serial.println(tarMinFinProg);*/
       if((((tiempoLeido.hora >= manHoraIniProg) && (tiempoLeido.hora <= manHoraFinProg)) && ((tiempoLeido.minuto >= manMinIniProg) && (tiempoLeido.minuto < manMinFinProg)))
         || (((tiempoLeido.hora >= tarHoraIniProg) && (tiempoLeido.hora <= tarHoraFinProg)) && ((tiempoLeido.minuto >= tarMinIniProg) && (tiempoLeido.minuto < tarMinFinProg))))
       {
